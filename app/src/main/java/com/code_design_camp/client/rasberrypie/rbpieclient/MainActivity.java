29cfc982385ae289231725939d2fda819245e0f7
Lukas Faber <luk.faber@gmx.de>
2018-08-30 17:42:51 +0200

    Added a SplashScreen and a plain MainActivity with a drawer

--- app/src/main/java/com/code_design_camp/client/rasberrypie/rbpieclient/MainActivity.java
+++ app/src/main/java/com/code_design_camp/client/rasberrypie/rbpieclient/MainActivity.java
@@ -1,7 +1,11 @@
 package com.code_design_camp.client.rasberrypie.rbpieclient;
 
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar t = findViewById(R.id.toolbar_main);
+        setSupportActionBar(t);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Snackbar.make(getCurrentFocus(),getString(R.string.leave_app),Snackbar.LENGTH_SHORT)
+                .setAction(getString(R.string.action_leave), new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        finishAffinity();
+                    }
+                }).show();
     }
 }
