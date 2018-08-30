22d460ed64cc13d7097d917710cb46a0d1efdbae
Lukas Faber <luk.faber@gmx.de>
2018-08-30 17:42:51 +0200

    Added a SplashScreen and a plain MainActivity with a drawer

--- app/src/main/java/com/code_design_camp/client/rasberrypie/rbpieclient/SplashScreen.java
+++ app/src/main/java/com/code_design_camp/client/rasberrypie/rbpieclient/SplashScreen.java
@@ -1,6 +1,8 @@
 package com.code_design_camp.client.rasberrypie.rbpieclient;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -13,6 +15,14 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(i);
+            }
+        },3000);
     }
 
 }
