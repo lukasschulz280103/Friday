6b4c697e28edb14131c371bcbe44a4fb09fd896f
laim2003 <luk.faber@gmx.de>
2018-09-01 14:00:42 +0200

    Initial commit

--- app/src/main/java/com/code_design_camp/client/friday/HeadDisplayClient/SplashScreen.java
+++ app/src/main/java/com/code_design_camp/client/friday/HeadDisplayClient/SplashScreen.java
@@ -1,13 +1,9 @@
-package com.code_design_camp.client.rasberrypie.rbpieclient;
+package com.code_design_camp.client.friday.HeadDisplayClient;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class SplashScreen extends AppCompatActivity {
 
