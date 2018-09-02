4c31e12fb730c223bf19e5667293057c8594da11
laim2003 <luk.faber@gmx.de>
2018-09-02 11:30:59 +0200

    Refactored VRFragment to use new created class DateWidget

--- app/src/main/java/com/code_design_camp/client/friday/HeadDisplayClient/view/DateWidget.java
+++ app/src/main/java/com/code_design_camp/client/friday/HeadDisplayClient/view/DateWidget.java
@@ -1,20 +1,50 @@
 package com.code_design_camp.client.friday.HeadDisplayClient.view;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
-import android.support.constraint.ConstraintSet;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class DateWidget extends TextWidget{
     public static final int TYPE_SIMPLE = 0;
     public static final int TYPE_MEDIUM = 1;
     public static final int TYPE_BIG = 2;
     private int relativeTop;
-    private ConstraintLayout parent;
+    private ViewGroup parent;
     private int relativeLeft;
     private int type;
-    public DateWidget(ConstraintLayout parent,int left, int top, int type){
+    private Context context;
+    public DateWidget(@NonNull Context context, @NonNull ViewGroup parent, int left, int top, int type){
+        super(context, parent, left, top);
         this.parent = parent;
         this.relativeLeft = left;
         this.relativeTop = top;
         this.type = type;
+        this.context = context;
+        Log.d("DateWidget","Context is "+context);
+    }
+
+    @Override
+    public void createWidget() {
+        final Date currentTime = Calendar.getInstance().getTime();
+        final TextWidget widget = new TextWidget(context,parent,relativeLeft,relativeTop);
+        widget.setText(currentTime.toString());
+        Timer updater = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                widget.setText(currentTime.toString());
+            }
+        };
+        int delay = 60-Calendar.getInstance().get(Calendar.SECOND);
+        updater.scheduleAtFixedRate(task,delay,60000);
+        widget.createWidget();
     }
 }
