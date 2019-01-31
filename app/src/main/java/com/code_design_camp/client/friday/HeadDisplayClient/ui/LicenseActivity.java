3abdafe8596430f03dc185018b723a14bc8a8bcb
Lukas Faber <luk.faber@gmx.de>
2019-01-31 23:12:52 +0100

    Added license view

--- app/src/main/java/com/code_design_camp/client/friday/HeadDisplayClient/ui/LicenseActivity.java
+++ app/src/main/java/com/code_design_camp/client/friday/HeadDisplayClient/ui/LicenseActivity.java
@@ -1,6 +1,9 @@
 package com.code_design_camp.client.friday.HeadDisplayClient.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +15,13 @@ public class LicenseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_license);
+    }
+
+    public void openLibraryPage(View v) {
+        String url = (String) v.getTag();
+        Intent openUrl = new Intent();
+        openUrl.setAction(Intent.ACTION_VIEW);
+        openUrl.setData(Uri.parse(url));
+        startActivity(openUrl);
     }
 }
