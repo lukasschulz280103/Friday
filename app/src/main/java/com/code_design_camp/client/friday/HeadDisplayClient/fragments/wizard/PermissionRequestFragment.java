0417561d18d2c77022524f5bbc2db7f99b86c795
Luke <luk.faber@gmx.de>
2019-02-13 10:30:37 +0100

    Fixed permission request

--- app/src/main/java/com/code_design_camp/client/friday/HeadDisplayClient/fragments/wizard/PermissionRequestFragment.java
+++ app/src/main/java/com/code_design_camp/client/friday/HeadDisplayClient/fragments/wizard/PermissionRequestFragment.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.PermissionChecker;
 import androidx.fragment.app.Fragment;
@@ -19,7 +20,9 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.code_design_camp.client.friday.HeadDisplayClient.R;
+import com.code_design_camp.client.friday.HeadDisplayClient.ui.WizardActivity;
 import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroBase;
 import com.github.paolorotolo.appintro.AppIntroFragment;
 import com.github.paolorotolo.appintro.ISlidePolicy;
 
@@ -30,6 +33,7 @@ import org.w3c.dom.Text;
  * A simple {@link Fragment} subclass.
  */
 public class PermissionRequestFragment extends Fragment implements ISlidePolicy {
+    String[] permissions = new String[]{Manifest.permission.RECORD_AUDIO,Manifest.permission.CAMERA};
     Button grant_perm;
     TextView warning;
 
@@ -46,32 +50,37 @@ public class PermissionRequestFragment extends Fragment implements ISlidePolicy 
         warning = v.findViewById(R.id.warn);
         grant_perm.setOnClickListener(view -> requestPermissions());
         if(isPolicyRespected()){
-            onActivityResult(100,Activity.RESULT_OK,null);
+            onRequestPermissionsResult(100,permissions,null);
+        }
+        else{
+
         }
         return v;
     }
     private void requestPermissions(){
-        String[] permissions = new String[]{Manifest.permission.RECORD_AUDIO,Manifest.permission.CAMERA};
-        getActivity().requestPermissions(permissions,3);
+        requestPermissions(permissions,3);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        int[] targetGrantResults = new int[]{PermissionChecker.PERMISSION_GRANTED,PermissionChecker.PERMISSION_GRANTED};
+        if(requestCode == 3){
+            if(!targetGrantResults.equals(grantResults)) {
+                grant_perm.setEnabled(false);
+                warning.setTextColor(Color.GREEN);
+                warning.setCompoundDrawablesWithIntrinsicBounds(getActivity().getDrawable(R.drawable.ic_check_green_24dp), null, null, null);
+                warning.setText(R.string.perms_granted);
+            }
+            else{
+
+            }
+        }else{
+            super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        }
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(requestCode == 100){
-            switch (resultCode) {
-                case Activity.RESULT_OK:{
-                    grant_perm.setEnabled(false);
-                    warning.setTextColor(Color.GREEN);
-                    warning.setCompoundDrawablesRelative(getActivity().getDrawable(R.drawable.ic_check_green_24dp),null,null,null);
-                    warning.setText(R.string.perms_granted);
-                }
-                case Activity.RESULT_CANCELED:{
-
-                }
-            }
-        }else{
-            super.onActivityResult(requestCode,resultCode,data);
-        }
     }
     @Override
     public boolean isPolicyRespected() {
